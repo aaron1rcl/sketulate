7de8f888adbaf1190b8395b6c0e6090e7c731140
Aaron Pickering <aaron_rcl@hotmail.com>
2026-02-07 01:31:10 +0100

    Update version number
    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sketulate',
-    version='0.1.0',
+    version='1.0',
     author='Aaron Pickering',
     author_email='aaron_rcl@hotmail.com',
     description='Sketchable function and density simulations for data science',
